1f6fc87e307c22c14956c8b04501cefb878334e5
larmor freq <100037095+cwlg102@users.noreply.github.com>
2022-04-27 21:15:33 +0900

    Add files via upload

---
+++ defcalculator.py
@@ -0,0 +1,119 @@
+def calculator(n):
+    
+    recur = ''
+    for i in range(len(n)): #(1 + 2 + 3) * 3 - > [1, 2, 3, 3] [(, +, +, ), *]
+        if i >= len(n):
+            break #n값이 계속 업뎃 되는데, 이때 range에넣은 len(n)값은 변하지않아서 i가 업데이트된 len(n)보다 클수가있음, 그래서 빼버림
+        if n[i] == '(':
+            for j in range(i, len(n)):
+                if n[j] == ')': #처음 괄호를 만나면, 일단은 포함해서 자름 
+                    a = n[:i]
+                    b = n[j+1:len(n)]
+                    recur = calculator(n[i+1:j+1])
+                    n = a + recur + b
+                    break # 재귀를 통해 괄호 만날때마다 괄호 안에꺼 계산하고감
+        
+        else: #1 + (2 + 3) = [1, 2, 3] [+, (, + ,)]
+            pass
+       
+    num = []
+    order = []
+    n = n.replace(')', '').strip() #문자열 입력이라 n을 얻을때 경우에 따라 ')'가 붙어나와서 괄호 기호들을 다 지워버림
+    if '--' in n:
+        n = n.replace('--', '+') #마이너스 두개는 플러스
+
+    for t, i in enumerate(n): #반복문을 통해 수식에있는 연산자를 순서대로 order 리스트에 넣어줌
+        if '+' in i:
+            order.append('+')
+        elif '-' in i:
+            if i == n[0] or n[t - 1] == '^' or n[t - 1] =='+' or n[t - 1] == '*' or n[t - 1] == '/' :
+                pass
+            else:
+                order.append('-')
+        elif '*' in i:
+            order.append('*')
+        elif '/' in i:
+            order.append('/')
+        elif '^' in i:
+            order.append('^')
+    
+
+    for i in range(len(n)):
+        if n[i] == '+': #숫자를 구분하기위해, 연산자를 모두 ';'로 바꿔버림
+            x = n[:i]
+            y = n[i+1:]
+            n = x + ';'+ y
+        elif (n[i] == '-' and i != 0) and (n[i] == '-' and n[i - 1] != ';'):
+            x = n[:i]
+            y = n[i+1:]
+            n = x + ';'+ y
+        elif n[i] == '*':
+            x = n[:i]
+            y = n[i+1:]
+            n = x + ';'+ y
+        elif n[i] == '/':
+            x = n[:i]
+            y = n[i+1:]
+            n = x + ';'+ y
+        elif n[i] == '^':
+            x = n[:i]
+            y = n[i+1:]
+            n = x + ';'+ y
+
+    temp = n.split(';') #숫자들을 temp리스트에 모아넣음(문자열)
+    if '' in temp:
+        temp.remove('')
+
+    for i in temp:
+        num.append(float(i)) #수식에 있는 숫자들을 순서대로 num 리스트에 넣어줌(부동소수점으로 변환)
+
+    if len(num) == 1:
+        order = [] #마이너스 관련 작업을 하다보면 order에 +만남아 들어갈수가 있음... 그래서 그거 없애주기
+
+    flag = 0 #0으로 나누는 경우 반복을 빠져나갈 깃발
+    iter = len(order)
+    for k in range(iter): #총 연산자 갯수만큼 연산을 하면 됨
+        for i in range(len(order)): #연산자의 숫자만큼 반복문을 통해 계산해줌, 제곱부터곱하기나누기더하기빼기 순으로 코딩하여 사칙연산 순서를 지키게함
+            if order[i] == '^':
+                num[i] = num[i] ** num[i + 1] #계산에 필요한 숫자의 인덱스와 해당 숫자로 계산하기위해 필요한 연산자의 인덱스는 일치함
+                num.pop(i+1) #remove로하면 겹치는 원소가 존재하면 해당원소 지우질 못함 ex) 2+3*6*2/2 (2가 여러개있는데 인덱스를 잡아줘도 값이 같으면 앞에꺼를 삭제해버림, 따라서 pop을써야)
+                order.remove('^') #연산자는 어차피 순서대로 계산이라 상관없음, 사용한 연산자는 리스트에서 삭제
+                break
+
+        for i in range(len(order)):
+            if order[i] == '*':
+                num[i] = num[i] * num[i + 1]
+                num.pop(i+1)
+                order.remove('*')
+                break
+
+        for i in range(len(order)):
+            if order[i] == '/':
+                if num[i + 1] == 0: 
+                    print('/////////////0으로 나눌수는 없어요/////////////')
+                    flag = 1
+                    break   
+                num[i] = num[i] / num[i + 1]
+                num.pop(i+1)
+                order.remove('/')
+                break
+        if flag: break  
+
+        for i in range(len(order)):
+            if order[i] == '+':
+                num[i] = num[i] + num[i + 1]
+                num.pop(i+1)
+                order.remove('+')
+                break
+        for i in range(len(order)):
+            if order[i] == '-':
+                num[i] = num[i] - num[i + 1]
+                num.pop(i+1) 
+                order.remove('-') 
+                break
+
+    return str(num[0])
+
+while(1):
+    n = input('숫자(연산자)숫자(연산자)숫자...로 입력해주세요: (띄어쓰기안됨)')
+    print(calculator(n))
